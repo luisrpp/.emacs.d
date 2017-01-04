2bb8fce3a8e3881e0f26f2388160824c6a5a82b8
Luis Roberto Pereira de Paula <luisrpp@gmail.com>
2017-01-04 00:29:16 -0200

    RVM + RSPEC modes

--- custom.el
+++ custom.el
@@ -5,10 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages
    (quote
-    (helm-swoop helm-ag helm-projectile helm evil-nerd-commenter dockerfile-mode docker evil-indent-textobject evil-jumper evil-leader neotree projectile evil-tabs evil-surround yaml-mode flycheck powerline-evil powerline rust-mode linum-relative python-mode markdown-mode magit fill-column-indicator evil emmet-mode clojure-mode use-package))))
+    (yaml-mode rust-mode rspec-mode rvm python-mode markdown-mode emmet-mode dockerfile-mode docker clojure-mode magit flycheck powerline neotree helm-swoop helm-ag helm-projectile helm evil-indent-textobject evil-surround evil-leader evil-nerd-commenter evil fill-column-indicator linum-relative solarized-theme monokai-theme dracula-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
