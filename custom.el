55b549a0b3b6b0b4a6d6841d051b7605500929da
Luis Roberto Pereira de Paula <luis.paula@vagas.com.br>
2017-01-06 11:29:56 -0200

    .

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
     ("c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" default)))
  '(package-selected-packages
    (quote
-    (rubocop yaml-mode rust-mode robe inf-ruby rspec-mode rvm python-mode markdown-mode emmet-mode dockerfile-mode docker clojure-mode magit yasnippet flycheck helm-company company neotree helm-swoop helm-ag helm-projectile helm evil-indent-textobject evil-surround evil-leader evil-nerd-commenter evil fill-column-indicator linum-relative monokai-theme solarized-theme dracula-theme use-package))))
+    (ag rubocop yaml-mode rust-mode robe inf-ruby rspec-mode rvm python-mode markdown-mode emmet-mode dockerfile-mode docker clojure-mode magit yasnippet flycheck helm-company company neotree helm-swoop helm-ag helm-projectile helm evil-indent-textobject evil-surround evil-leader evil-nerd-commenter evil fill-column-indicator linum-relative monokai-theme solarized-theme dracula-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
