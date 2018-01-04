daaf41b932fbe4fa0d6e71d76be10674aa31d5ba
Luis Roberto Pereira de Paula <luis.paula@vagas.com.br>
2018-01-04 10:25:35 -0200

    Adding ace jump

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
     ("8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" default)))
  '(package-selected-packages
    (quote
-    (yaml-mode markdown-mode emmet-mode sml-mode clojure-mode python-mode robe inf-ruby rspec-mode rvm dockerfile-mode docker magit flycheck neotree helm-projectile projectile dired+ helm-ag ag helm fill-column-indicator monokai-theme color-theme-solarized diminish use-package))))
+    (ace-window ace-jump-zap ace-jump-mode yaml-mode markdown-mode emmet-mode sml-mode clojure-mode python-mode robe inf-ruby rspec-mode rvm dockerfile-mode docker magit flycheck neotree helm-projectile projectile dired+ helm-ag ag helm fill-column-indicator monokai-theme color-theme-solarized diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
