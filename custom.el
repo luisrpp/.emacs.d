36a6a359e20d9384b17b1c25bb6efe0589c368fe
Luis Roberto P. Paula <luisrpp@gmail.com>
2018-01-09 00:36:26 -0200

    Removing python mode

--- custom.el
+++ custom.el
@@ -5,10 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" default)))
+    ("53f97243218e8be82ba035ae34c024fd2d2e4de29dc6923e026d5580c77ff702" default)))
  '(package-selected-packages
    (quote
-    (ace-window ace-jump-zap ace-jump-mode yaml-mode markdown-mode emmet-mode sml-mode clojure-mode python-mode robe inf-ruby rspec-mode rvm dockerfile-mode docker magit flycheck neotree helm-projectile projectile dired+ helm-ag ag helm fill-column-indicator monokai-theme color-theme-solarized diminish use-package))))
+    (yaml-mode markdown-mode emmet-mode sml-mode clojure-mode robe inf-ruby rspec-mode rvm dockerfile-mode docker magit flycheck neotree helm-projectile projectile dired+ helm-ag ag helm ace-window ace-jump-zap ace-jump-mode fill-column-indicator monokai-theme color-theme-solarized diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
