a8791b770d76425c6b2b67ba95bdae4a5310f2a7
Luis Roberto P. Paula <luisrpp@gmail.com>
2018-01-10 00:22:47 -0200

    Config changes

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
     ("53f97243218e8be82ba035ae34c024fd2d2e4de29dc6923e026d5580c77ff702" default)))
  '(package-selected-packages
    (quote
-    (yaml-mode markdown-mode emmet-mode sml-mode clojure-mode robe inf-ruby rspec-mode rvm dockerfile-mode docker magit flycheck neotree helm-projectile projectile dired+ helm-ag ag helm ace-window ace-jump-zap ace-jump-mode fill-column-indicator monokai-theme color-theme-solarized diminish use-package))))
+    (yaml-mode markdown-mode emmet-mode sml-mode clojure-mode go-mode robe inf-ruby rspec-mode rvm dockerfile-mode docker magit flycheck neotree powerline helm-company company helm-projectile projectile dired+ helm-ag ag helm ace-window ace-jump-zap ace-jump-mode fill-column-indicator monokai-theme diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
