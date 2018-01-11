7713f262c29f74576b39dea85aebfafb7436f708
Luis Roberto P. Paula <luisrpp@gmail.com>
2018-01-10 23:57:03 -0200

    Config changes

--- custom.el
+++ custom.el
@@ -3,12 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("53f97243218e8be82ba035ae34c024fd2d2e4de29dc6923e026d5580c77ff702" default)))
  '(package-selected-packages
    (quote
-    (yaml-mode markdown-mode emmet-mode sml-mode clojure-mode go-mode robe inf-ruby rspec-mode rvm dockerfile-mode docker magit flycheck neotree powerline helm-company company helm-projectile projectile dired+ helm-ag ag helm ace-window ace-jump-zap ace-jump-mode fill-column-indicator monokai-theme diminish use-package))))
+    (yaml-mode markdown-mode emmet-mode sml-mode clojure-mode go-mode robe inf-ruby rspec-mode rvm dockerfile-mode docker magit flycheck neotree helm-company company helm-projectile projectile dired+ helm-ag ag helm ace-window ace-jump-zap ace-jump-mode fill-column-indicator powerline atom-one-dark-theme diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
