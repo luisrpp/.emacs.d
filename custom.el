93864b47c954a0dbc5682ed39dc068065a548364
Luis Roberto P. Paula <luisrpp@gmail.com>
2018-01-13 11:50:20 -0200

    Config changes

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yaml-mode markdown-mode emmet-mode sml-mode clojure-mode go-mode robe inf-ruby rspec-mode rvm dockerfile-mode docker magit flycheck neotree helm-company company helm-projectile projectile dired+ helm-ag ag helm ace-window ace-jump-zap ace-jump-mode fill-column-indicator powerline atom-one-dark-theme diminish use-package))))
+    (exec-path-from-shell yaml-mode markdown-mode emmet-mode sml-mode clojure-mode go-mode robe inf-ruby rspec-mode rvm dockerfile-mode docker magit flycheck neotree helm-company company helm-projectile projectile dired+ helm-ag ag helm ace-window ace-jump-zap ace-jump-mode fill-column-indicator powerline atom-one-dark-theme diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
