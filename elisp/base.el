d933df0c85ca829a197383380e9c340cd7f69609
Luis Roberto Pereira de Paula <luisrpp@gmail.com>
2018-11-25 16:34:29 -0200

    Update base.el

--- elisp/base.el
+++ elisp/base.el
@@ -34,57 +34,57 @@
 (setq default-process-coding-system '(utf-8-unix . utf-8-unix))
 
 ;; Emacs customizations
-(setq confirm-kill-emacs                  'y-or-n-p
-      confirm-nonexistent-file-or-buffer  t
+(setq confirm-kill-emacs                   'y-or-n-p
+      confirm-nonexistent-file-or-buffer     t
       save-interprogram-paste-before-kill t
       mouse-yank-at-point                 t
-      require-final-newline               t
+      require-final-newline                t
       visible-bell                        nil
       ring-bell-function                  'ignore
-      custom-file                         "~/.emacs.d/.custom.el"
+      custom-file                          "~/.emacs.d/.custom.el"
       ;; http://ergoemacs.org/emacs/emacs_stop_cursor_enter_prompt.html
       minibuffer-prompt-properties
       '(read-only t point-entered minibuffer-avoid-prompt face minibuffer-prompt)
 
       ;; Disable non selected window highlight
-      cursor-in-non-selected-windows     nil
-      highlight-nonselected-windows      nil
+      cursor-in-non-selected-windows      nil
+      highlight-nonselected-windows       nil
 
       ;; PATH
-      exec-path                          (append exec-path '("/usr/local/bin/"))
-      indent-tabs-mode                   nil
-      tab-width                          2
-      inhibit-startup-message            t
-      fringes-outside-margins            t
-      select-enable-clipboard            t
-      select-enable-primary              t
-      mouse-drag-copy-region             t
+      exec-path                           (append exec-path '("/usr/local/bin/"))
+      indent-tabs-mode                    nil
+      tab-width                           2
+      inhibit-startup-message             t
+      fringes-outside-margins             t
+      select-enable-clipboard             t
+      select-enable-primary               t
+      mouse-drag-copy-region              t
 
       ;; Scroll smoothly
-      scroll-step                        1
-      scroll-conservatively              10
-      scroll-margin                      7
-      scroll-conservatively              5)
+      scroll-step                         1
+      scroll-conservatively               10
+      scroll-margin                       7
+      scroll-conservatively               5)
 
-(defvar use-package-always-ensure        t)
+(defvar use-package-always-ensure         t)
 
 ;; Turn off the blinking cursor
-(blink-cursor-mode -1)
+(blink-cursor-mode                        -1)
 
 ;; Bookmarks
-(defvar bookmark-save-flag               t)
-(defvar bookmark-default-file            (concat temp-dir "/bookmarks"))
+(defvar bookmark-save-flag                 t)
+(defvar bookmark-default-file              (concat temp-dir "/bookmarks"))
 
 ;; Backups enabled, use nil to disable
-(setq history-length                     1000
-      backup-inhibited                   nil
-      make-backup-files                  t
-      auto-save-default                  t
-      auto-save-list-file-name           (concat temp-dir "/autosave")
-      make-backup-files                  t
-      create-lockfiles                   nil
-      backup-directory-alist            `((".*" . ,(concat temp-dir "/backup/")))
-      auto-save-file-name-transforms    `((".*" ,(concat temp-dir "/auto-save-list/") t)))
+(setq history-length                      1000
+      backup-inhibited                    nil
+      make-backup-files                    t
+      auto-save-default                   t
+      auto-save-list-file-name             (concat temp-dir "/autosave")
+      make-backup-files                    t
+      create-lockfiles                     nil
+      backup-directory-alist              `((".*" . ,(concat temp-dir "/backup/")))
+      auto-save-file-name-transforms       `((".*" ,(concat temp-dir "/auto-save-list/") t)))
 
 (fset 'yes-or-no-p 'y-or-n-p)
 (global-auto-revert-mode t)
@@ -122,4 +122,4 @@
 (require 'diminish)
 
 (provide 'base)
-;;; base ends here
+;;; base.el ends here
