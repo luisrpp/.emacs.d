301f90a9797a4d96e85dd2601d3e6dcf185c7d71
Luis Roberto P. Paula <luisrpp@gmail.com>
2018-11-25 16:38:07 -0200

    UPDATE base.el

--- elisp/base.el
+++ elisp/base.el
@@ -34,14 +34,14 @@
 (setq default-process-coding-system '(utf-8-unix . utf-8-unix))
 
 ;; Emacs customizations
-(setq confirm-kill-emacs                   'y-or-n-p
-      confirm-nonexistent-file-or-buffer     t
+(setq confirm-kill-emacs                  'y-or-n-p
+      confirm-nonexistent-file-or-buffer  t
       save-interprogram-paste-before-kill t
       mouse-yank-at-point                 t
-      require-final-newline                t
+      require-final-newline               t
       visible-bell                        nil
       ring-bell-function                  'ignore
-      custom-file                          "~/.emacs.d/.custom.el"
+      custom-file                         "~/.emacs.d/.custom.el"
       ;; http://ergoemacs.org/emacs/emacs_stop_cursor_enter_prompt.html
       minibuffer-prompt-properties
       '(read-only t point-entered minibuffer-avoid-prompt face minibuffer-prompt)
@@ -72,19 +72,19 @@
 (blink-cursor-mode                        -1)
 
 ;; Bookmarks
-(defvar bookmark-save-flag                 t)
-(defvar bookmark-default-file              (concat temp-dir "/bookmarks"))
+(defvar bookmark-save-flag                t)
+(defvar bookmark-default-file             (concat temp-dir "/bookmarks"))
 
 ;; Backups enabled, use nil to disable
 (setq history-length                      1000
       backup-inhibited                    nil
-      make-backup-files                    t
+      make-backup-files                   t
       auto-save-default                   t
-      auto-save-list-file-name             (concat temp-dir "/autosave")
-      make-backup-files                    t
-      create-lockfiles                     nil
+      auto-save-list-file-name            (concat temp-dir "/autosave")
+      make-backup-files                   t
+      create-lockfiles                    nil
       backup-directory-alist              `((".*" . ,(concat temp-dir "/backup/")))
-      auto-save-file-name-transforms       `((".*" ,(concat temp-dir "/auto-save-list/") t)))
+      auto-save-file-name-transforms      `((".*" ,(concat temp-dir "/auto-save-list/") t)))
 
 (fset 'yes-or-no-p 'y-or-n-p)
 (global-auto-revert-mode t)
